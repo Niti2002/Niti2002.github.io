f35c172e3ab5539e87fed6b82e2a6683a0a5b4ff
Niti Dewangan <nitidewangan20@gmail.com>
2024-08-14 23:01:56 +0530

    Gym Website

---
+++ GYM WEBSITE/java.js
@@ -0,0 +1,5 @@
+function toggleModal() {
+    var modal = document.getElementById("joinUsModal");
+    modal.style.display = (modal.style.display === "none" || modal.style.display === "") ? "flex" : "none";
+}
+
